bf9b8060246af12455da96a80255e911b85b39d6
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-05-30 14:02:00 -0300

    manejo de errores de db

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection;
 
 db.on("error", (error) => {
+  console.error("Database connection error:", error);
+});
+db.once("open", () => {
+  console.log("Database connected successfully");
+});
+
+db.on("error", (error) => {
   console.log(error);
 });
 db.once("open", () => {
