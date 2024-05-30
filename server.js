7da8d58a2accf88bd1c01fac54c38c0258efcea2
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-05-30 14:57:45 -0300

    Se agrega path también a public

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/health", (req, res) => {
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
